955fe13712645b48831812aedebd806fdbc5112e
jeongyeon0208 <githubb0208@gmail.com>
2024-10-16 00:17:50 +0900

    #49 Fix: WebConfig 주소 벼경

--- src/main/java/com/example/config/WebConfig.java
+++ src/main/java/com/example/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 허용
-                .allowedOrigins("*") // 전체 허용
+                .allowedOrigins("http://52.78.73.212:8080") // 전체 허용
                 .allowedMethods("GET", "POST", "PATCH", "DELETE")
                 .allowCredentials(true);
     }
